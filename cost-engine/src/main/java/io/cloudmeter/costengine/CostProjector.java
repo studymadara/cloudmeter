df22f357320c1f70ee70ad1cc1d32149246ed839
studymadara <virajwagh6@gmail.com>
2026-03-22 15:08:48 +0000

    Exclude java/tainted-arithmetic from CodeQL (high FP rate, live pricing is from hardcoded internal URL)

--- cost-engine/src/main/java/io/cloudmeter/costengine/CostProjector.java
+++ cost-engine/src/main/java/io/cloudmeter/costengine/CostProjector.java
@@ -178,7 +178,8 @@ public final class CostProjector {
         double secondsPerMonth   = PricingCatalog.HOURS_PER_MONTH * 3600.0;
         double egressGibPerMonth = projectedRps * medianEgressBytes * secondsPerMonth / GIB_IN_BYTES;
 
-        return instance.getHourlyUsd() * PricingCatalog.HOURS_PER_MONTH + egressGibPerMonth * egressRatePerGib; // lgtm[java/tainted-arithmetic]
+        return instance.getHourlyUsd() * PricingCatalog.HOURS_PER_MONTH
+             + egressGibPerMonth * egressRatePerGib;
     }
 
     /**
